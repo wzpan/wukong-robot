bac414f671d9cce6958f82a183af5b1b898176f7
josephpan <m@hahack.com>
2019-02-04 21:51:48 +0800

    feat(Email): 增加发邮件的功能

--- robot/utils.py
+++ robot/utils.py
@@ -7,10 +7,89 @@ import struct
 import shutil
 import re
 import time
+import logging
 from . import constants, config
 from pydub import AudioSegment
+from pytz import timezone
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+logger = logging.getLogger(__name__)
 
 do_not_bother = False
+
+def sendEmail(SUBJECT, BODY, ATTACH_LIST, TO, FROM, SENDER,
+              PASSWORD, SMTP_SERVER, SMTP_PORT):
+    """Sends an email."""
+    txt = MIMEText(BODY.encode('utf-8'), 'html', 'utf-8')
+    msg = MIMEMultipart()
+    msg.attach(txt)    
+
+    for attach in ATTACH_LIST:
+        try:
+            att = MIMEText(open(attach, 'rb').read(), 'base64', 'utf-8')
+            filename = os.path.basename(attach)
+            att["Content-Type"] = 'application/octet-stream'
+            att["Content-Disposition"] = 'attachment; filename="%s"' % filename
+            msg.attach(att)
+        except Exception:
+            logger.error(u'附件 %s 发送失败！' % attach)
+            continue
+
+    msg['From'] = SENDER
+    msg['To'] = TO
+    msg['Subject'] = SUBJECT
+
+    try:
+        session = smtplib.SMTP()
+        session.connect(SMTP_SERVER, SMTP_PORT)
+        session.starttls()
+        session.login(FROM, PASSWORD)
+        session.sendmail(SENDER, TO, msg.as_string())
+        session.close()
+        return True
+    except Exception as e:
+        logger.error(e)
+        return False
+
+
+def emailUser(profile, SUBJECT="", BODY="", ATTACH_LIST=[]):
+    """
+    sends an email.
+
+    Arguments:
+        profile -- contains information related to the user (e.g., email
+                   address)
+        SUBJECT -- subject line of the email
+        BODY -- body text of the email
+    """
+    # add footer
+    if BODY:
+        BODY = u"%s，<br><br>这是您要的内容：<br>%s<br>" % (profile['first_name'], BODY)
+
+    recipient = profile['email']['address']
+    robot_name = u'叮当'
+    if profile['robot_name_cn']:
+        robot_name = profile['robot_name_cn']
+    recipient = robot_name + " <%s>" % recipient
+
+    if not recipient:
+        return False
+
+    try:
+        user = profile['email']['address']
+        password = profile['email']['password']
+        server = profile['email']['smtp_server']
+        port = profile['email']['smtp_port']
+        sendEmail(SUBJECT, BODY, ATTACH_LIST, user, user,
+                  recipient, password, server, port)
+
+        return True
+    except Exception as e:
+        logger.error(e)
+        return False
+
 
 def get_file_content(filePath):
     """ 读取文件 """
@@ -86,4 +165,4 @@ def get_do_not_bother_off_hotword():
 
 def getTimezone():
     """ 获取时区 """
-    return config.get('timezone', 'HKT')
+    return timezone(config.get('timezone', 'HKT'))
