c6fd8bb6be8fffd8e52ef335326b14de6381e0be
Joseph Pan <m@hahack.com>
2019-02-22 01:12:16 +0800

    chore(docs): update docs

--- robot/NLU.py
+++ robot/NLU.py
@@ -25,26 +25,68 @@ class AbstractNLU(object):
 
     @abstractmethod
     def parse(self, query, **args):
-        return None        
+        """
+        进行 NLU 解析
+
+        :param query: 用户的指令字符串
+        :param **args: 可选的参数
+        """
+        return None
 
     @abstractmethod
     def getIntent(self, parsed):
+        """ 
+        提取意图
+
+        :param parsed: 解析结果
+        :returns: 意图数组
+        """
         return None
 
     @abstractmethod
     def hasIntent(self, parsed, intent):
+        """ 
+        判断是否包含某个意图
+
+        :param parsed: 解析结果
+        :param intent: 意图的名称
+        :returns: True: 包含; False: 不包含
+        """
         return False
 
     @abstractmethod
     def getSlots(self, parsed, intent):
+        """ 
+        提取某个意图的所有词槽
+    
+        :param parsed: 解析结果
+        :param intent: 意图的名称
+        :returns: 词槽列表。你可以通过 name 属性筛选词槽，
+        再通过 normalized_word 属性取出相应的值
+        """
         return None
 
     @abstractmethod
-    def getSlotWord(self, parsed, intent, name):
+    def getSlotWords(self, parsed, intent, name):
+        """ 
+        找出命中某个词槽的内容
+    
+        :param parsed: 解析结果
+        :param intent: 意图的名称
+        :param name: 词槽名
+        :returns: 命中该词槽的值的列表。
+        """
         return None
 
     @abstractmethod
     def getSay(self, parsed, intent):
+        """
+        提取回复文本
+
+        :param parsed: 解析结果
+        :param intent: 意图的名称
+        :returns: 回复文本
+        """
         return ""
 
 
