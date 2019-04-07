9d333b1b103ebe179371c2883f21ab8f012c739c
josephpan <m@hahack.com>
2019-04-07 17:17:34 +0800

    fix(Geek): fix pyflakes error

--- plugins/Geek.py
+++ plugins/Geek.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8-*-
-import os
-from robot import config, logging
+from robot import logging
 from robot.sdk.AbstractPlugin import AbstractPlugin
 
 logger = logging.getLogger(__name__)
