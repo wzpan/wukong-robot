f75f1c47e9b85e28380c3f15da37ec9e1b672e07
wzpan <m@hahack.com>
2023-02-18 16:50:17 +0800

    feat: 聊天记录支持换行展示

--- server/static/index.js
+++ server/static/index.js
@@ -11,25 +11,26 @@ function appendHistory(type, query, uuid, plugin) {
               </div>
 `);
     } else {
+        queries = query.split('\n');
+        $('.history').append(`
+          <div class="left">
+             <div class="bubble bubble-white">
+               <div class="bubble-avatar"><image src="./static/robot.png" width=32px attr="robot" /></div>
+               <div style="text-align: left" id="${uuid}">
+             </div>
+           </div>
+        `);
+        for (i in queries) {
+            console.log(`#${uuid}`);
+            let line = queries[i].replaceAll(' ', '&nbsp;')
+            $(`#${uuid}`).append(`
+                <p>${line}</p>
+            `);
+        }
         if (plugin) {
-            $('.history').append(`
-              <div class="left">
-                 <div class="bubble bubble-white">
-                   <div class="bubble-avatar"><image src="./static/robot.png" width=32px attr="robot" /></div>
-                   <p style="text-align: left" id="${uuid}">${query}</p>
-                   <span class="badge badge-info plugin">${plugin}</span>
-                 </div>
-              </div>
-`);
-        } else {        
-            $('.history').append(`
-              <div class="left">
-                 <div class="bubble bubble-white">
-                   <div class="bubble-avatar"><image src="./static/robot.png" width=32px attr="robot" /></div>
-                   <p style="text-align: left" id="${uuid}">${query}</p>
-                 </div>
-              </div>
-`);        
+            $(`#${uuid}`).after(`
+               <span class="badge badge-info plugin">${plugin}</span>             
+            `);
         }
     }
     $("#"+uuid).hide();
